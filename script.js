256eccae73f6128373313d97f387ff65d3257cf4
karen queiroz7 <kqueiroz812@gmail.com>
2019-07-19 16:48:47 -0300

    adicionar exercicio um

--- script.js
+++ script.js
@@ -14,6 +14,27 @@ if(idade >= 60 || aposentado === "sim"){
         alert("MEIA ENTRADA")
     }
     else{
-        alert("ENTADA 35 REAIS");
+        alert("ENTRADA 35 REAIS");
     }
 }
+
+
+
+
+let idade = Number (prompt("Digite sua idade"));
+
+    if(idade >=25 && idade <= 60){
+       let salario= Number(prompt("Digite seu salario"));
+          if (salario >=2000){
+             let valor = Number(prompt("Digite o valor do emprestimo desejado"))
+                if(valor <= 10*salario){
+                   console.log("Emprestimo aprovado")
+                }else{
+                   console.log("Valor nao permitido")
+                }  
+            }else{
+            console.log("Salario nao permitido para o emprestimo")
+            }
+        }else{
+        console.log("Idade nao permitida para emprestimo")
+    }
